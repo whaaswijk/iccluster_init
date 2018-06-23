b0291b8b9e2c788a0773ada59c344945d4f859f8
Winston Haaswijk <winston.haaswijk@gmail.com>
2018-06-22 20:59:36 -0700

    Adding gdb and valgrind

--- init.sh
+++ init.sh
@@ -3,5 +3,5 @@ apt update
 apt -y upgrade
 apt install -y gcc g++ cmake git mercurial automake bash-completion vim
 apt install -y libreadline-dev zlib1g-dev libboost-all-dev screen
-apt install -y libnauty2-dev
+apt install -y libnauty2-dev gdb valgrind
 
